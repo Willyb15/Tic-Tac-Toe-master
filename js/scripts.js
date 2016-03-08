f426ba394c4ecd529ef8b47e0a7a9777ed791548
Willyb15 <william.steen.bryant@gmail.com>
2016-03-08 12:10:33 -0500

    Class WalkThrough
    Writing code we used in Class for Tic Tac Toe game

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,35 @@
-function startGame(){
-	if(Math.random() < 0.5){
-	 	document.turn = "O";
-	} else{
-		document.turn = "X";
+var winners =[
+	["a1", "a2", "a3"],
+	["c1","c2", "c3"],
+	["b1", "b2", "b3"],
+	["a1","b1", "c1"],
+	["a2","b2","c3"],
+	["a3","b3","c3"],
+	["a1","b2","c3"]
+];
+var whosTurn = 1;
+
+function addSymbol(element){
+	var status = document.getElementById("status");
+	if(element.innerHTML == ""){
+		status.className = "";
+		if(whosTurn ==1) {
+			element.innerHTML = "X"
+			whosTurn = 2; 
+			status.innerHTML = "It's O's turn";
+			status.className = "green";
+		} else{
+			element.innerHTML = "O"
+			whosTurn = 1;
+			status.innerHTML = "It's X's turn";
+			status.className = "pink";
+		}
+		} else{
+			var status = document.getElementById("status");
+			status.innerHTML = "This box is taken";
+		// status.style.backgroundColor = "red";
+			status.className = "red";
 	}
-	setStatus(document.turn + " gets to start.");
-}
-
-function setStatus(msg){
-	document.getElementById("status").innerHTML = msg;
-}
-
-function nextMove(tile){
-	if(tile.innerHTML === ""){
-		tile.innerHTML = document.turn
-		switchTurn();
-	} else{
-		setStatus("That sqaure is taken!!");
-	}
-}
-
-function switchTurn(){
-	if(document.turn === "X"){
-		document.turn = "O";
-		setStatus("It's " + document.turn + " turn");
-	} else {
-		document.turn = "X";
-		setStatus("It's " + document.turn + " turn");
-	}
-}
-
-function clearGame(){
-	 location.reload();
 }
 
 
@@ -40,5 +38,25 @@ function clearGame(){
 
 
 
+
+
+
+
+
+
+// console.log(winners[0],[0]);
+// console.log(winners[0][0]);
+
+var squareWidth = document.getElementById("a1").clientWidth;
+// console.log(squareWidth);
+var squares = document.getElementsByClassName("square");
+// console.dir(squares);
+
+for(i=0; i<squares.length;i++){
+	squares[i].style.height = squareWidth + "px";	
+}
+
+// console.log(squares[i]);
+// console.log(squares[i].style.height);
 
 
