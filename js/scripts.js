b973341d20c3403291dac532514b921e087f9813
Willyb15 <william.steen.bryant@gmail.com>
2016-03-08 15:23:59 -0500

    Got JavaScript working

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-var winners =[
+
+var winners = [
 	["a1", "a2", "a3"],
 	["c1","c2", "c3"],
 	["b1", "b2", "b3"],
@@ -6,8 +7,10 @@ var winners =[
 	["a2","b2","c3"],
 	["a3","b3","c3"],
 	["a1","b2","c3"]
-];
+	];
 var whosTurn = 1;
+var playerOneMarkings = [];
+var playerTwoMarkings = [];
 
 function addSymbol(element){
 	var status = document.getElementById("status");
@@ -18,21 +21,64 @@ function addSymbol(element){
 			whosTurn = 2; 
 			status.innerHTML = "It's O's turn";
 			status.className = "green";
+			// element.classList.remove('empty');
+			// element.classList.add = ("p1");
+			playerOneMarkings.push(element.id);
 		} else{
 			element.innerHTML = "O"
 			whosTurn = 1;
 			status.innerHTML = "It's X's turn";
 			status.className = "pink";
-		}
+			// element.classList.remove('empty');
+			// element.classList.add("p2");
+			playerTwoMarkings.push(element.id);
+			}
 		} else{
 			var status = document.getElementById("status");
 			status.innerHTML = "This box is taken";
 		// status.style.backgroundColor = "red";
 			status.className = "red";
 	}
+	checkWin();
+}
+
+function checkWin(){
+	console.log(playerTwoMarkings);
+	console.log(playerOneMarkings);
+	//Define a variable, and if it gets to 3, then we have a winner. If it doesn't, the row is not complete
+	var rowCount = 0;
+	var thisWinCombination;
+	//Loop through all winning possibilities. Rowcount needs to restart each time.
+	for(i=0; i<winners.length; i++){
+		rowCount=0;
+		thisWinCombination = winners[i];
+	//Now, let's check if all the elements in the winners array, exists in the current player and
+		for(j=0; j<thisWinCombination.length; j++){
+			//Check if this square of the win combo we are on, is in the players marking.
+			if(playerOneMarkings.indexOf(thisWinCombination[j]) > -1){
+				//Hit
+				//rowCount = rowCount + 1;
+				rowCount++;
+			}
+		}
+		if(rowCount===3){
+			//Sombody won!!!
+			gameOver(thisWinCombination);
+		}
+	}
 }
 
 
+function gameOver(combo){
+	var comboString;
+	var status = document.getElementById("status");
+	for(i=0; i<combo.length; i++){
+		comboString += combo[i];
+		document.getElementById(combo[i]).classList.add("winner");
+
+	}
+	status.innerHTML ="Player 1 won the game with: " + comboString;
+}
 
 
 
