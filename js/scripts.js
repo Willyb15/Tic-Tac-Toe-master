5b15673966b0ad6837455989c1470e8618431365
Willyb15 <william.steen.bryant@gmail.com>
2016-03-07 22:57:38 -0500

    Updated HTML and JS
    Added JS and updated HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
+function startGame(){
+	document.turn = "X"
+	setMessage(document.turn + " gets to start.");
+	}
+function setMessage(msg){
+	document.getElementById("status").innerHTML = msg;
+	}
 
+function nextMove(tile){
+	tile.innerHTML = document.turn
+	switchTurn();
+}
+
+function switchTurn(){
+	if(document.turn === "X"){
+	document.turn = "O";
+	setMessage("It's " + document.turn + " turn");
+	} else {
+	document.turn = "X";
+	setMessage("It's " + document.turn + " turn");
+	}
+}
 
 
 
