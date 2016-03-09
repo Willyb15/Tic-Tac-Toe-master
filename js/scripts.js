1c7fcc290a0bab12854671bb47fb102e9c675f08
Willyb15 <william.steen.bryant@gmail.com>
2016-03-09 08:42:52 -0500

    learning how to update from master.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 function startGame(){
 	if(Math.random() < 0.5){
 	 	document.turn = "O";
@@ -12,27 +13,61 @@ function setStatus(msg){
 }
 
 function nextMove(tile){
+	var status = document.getElementById("status");
 	if(tile.innerHTML === ""){
 		tile.innerHTML = document.turn
 		switchTurn();
 	} else{
 		setStatus("That sqaure is taken!!");
+		status.className = "red";
 	}
 }
 
 function switchTurn(){
+	var status = document.getElementById("status");
 	if(document.turn === "X"){
 		document.turn = "O";
 		setStatus("It's " + document.turn + " turn");
+		status.className = "white";
 	} else {
 		document.turn = "X";
 		setStatus("It's " + document.turn + " turn");
+		status.className = "green";
 	}
 }
 
 function clearGame(){
 	 location.reload();
 }
+
+
+
+// function addSymbol(element){
+// 	var status = document.getElementById("status");
+// 	if(element.innerHTML == ""){
+// 		status.className = "";
+// 		if(whosTurn ==1) {
+// 			element.innerHTML = "X"
+// 			whosTurn = 2; 
+// 			status.innerHTML = "It's O's turn";
+// 			status.className = "green";
+// 		} else{
+// 			element.innerHTML = "O"
+// 			whosTurn = 1;
+// 			status.innerHTML = "It's X's turn";
+// 			status.className = "pink";
+// 		}
+// 		} else{
+// 			var status = document.getElementById("status");
+// 			status.innerHTML = "This box is taken";
+// 		// status.style.backgroundColor = "red";
+// 			status.className = "red";
+// 	}
+// }
+
+
+
+
 
 
 
